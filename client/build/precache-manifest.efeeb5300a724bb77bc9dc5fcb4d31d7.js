9181ecd62af754664a827131dc177c5e9c14cc35
Micah <mjayfinley@gmail.com>
2018-12-18 15:00:27 -0600

    heroku

---
+++ client/build/precache-manifest.efeeb5300a724bb77bc9dc5fcb4d31d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "268b5fa24c2b61945e2a",
+    "url": "/static/js/main.268b5fa2.chunk.js"
+  },
+  {
+    "revision": "6848242f4971d970ce06",
+    "url": "/static/js/1.6848242f.chunk.js"
+  },
+  {
+    "revision": "268b5fa24c2b61945e2a",
+    "url": "/static/css/main.6adf5ade.chunk.css"
+  },
+  {
+    "revision": "04e32fb168a495345781251ef8283792",
+    "url": "/index.html"
+  }
+];
