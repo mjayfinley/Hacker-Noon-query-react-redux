28942af5a03b37675d33b15e9788e77d7a348691
Micah <mjayfinley@gmail.com>
2018-12-19 12:02:49 -0600

    hosting

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const CORS_WHITELIST = require('./constants/frontend')
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://lit-taiga-65019.herokuapp.com/',
   optionSuccessStatus: 200
 }
 
